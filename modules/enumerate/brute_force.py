50488db83773e158d6ef470a87e49e52fd20a1a5
WMDA <p3dh0001@gmail.com>
2021-11-14 23:23:30 +0000

    started on brute_force.py

---
+++ modules/enumerate/brute_force.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import os
+
+
+def load_json():
+    with open('config.json') as f:
+        data= json.load(f)
+
+    return(data)
+
+data =load_json()
+
+
+
+sub_list = open(os.path.join(os.getcwd(),data['wordlist_location'], data['subdomain_wordlist'])).read()
