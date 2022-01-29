e7cac84ccbaddb32042b1f180d422e5417176f7f
WMDA <p3dh0001@gmail.com>
2022-01-29 12:51:55 -0500

    made code more readable

--- weapy.py
+++ weapy.py
@@ -16,9 +16,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         
         colours = colors()
-        
         print(colours['WARNING'] + '\nUser initiated shutdown' + colours['RESET'])
-        
         print(colours['BLUE'] + '\nBYE!!' + colours['RESET'])
-        
         sys.exit(0)
