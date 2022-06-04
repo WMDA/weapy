1525234abc071b6d9cc7986648cbbd1db10399ea
wmda <p3dh0001@gmail.com>
2022-06-04 15:02:19 +0100

    updated file option

--- modules/file/file.py
+++ modules/file/file.py
@@ -1,16 +1,33 @@
 import re
-
+from weapy.weapy_class import WeaPy
 
 def directories(filename):
     dirs = []
     with open(filename) as f:
         content = f.read().splitlines()
         for line in content:
-            file = re.findall(r'^/.*?\W', line)
+            file = re.findall(r'^.*?\s', line)
             dirs.append(file)
     return dirs
 
-def url(filename, url):
+def get_url(filename, url):
+    urls= []
     dir = directories(filename)
     for directory in dir:
-        print(url + directory[0])
+        urls.append(url + directory[0])
+
+    return urls
+
+def file_modules(args):
+    url = args['url']
+    filename = args['file']
+    urls = get_url(filename, url) 
+    for ip in urls:
+        args.update({'url':ip})
+        WeaPy(args)
+        
+
+
+
+
+
--- weapy.py
+++ weapy.py
@@ -5,17 +5,32 @@ import sys
 from weapy.arguments import arguments
 from modules.prettify.colours import colors
 from weapy.weapy_class import WeaPy
+from modules.file.file import file_modules  
 
 if __name__ == "__main__":
     
     arg = arguments()
     
-    try:
-        WeaPy(arg)
+    if arg['file'] != False:
 
-    except KeyboardInterrupt:
+        try:
+            file_modules(arg)
         
-        colours = colors()
-        print(colours['WARNING'] + '\nUser initiated shutdown' + colours['RESET'])
-        print(colours['BLUE'] + '\nBYE!!' + colours['RESET'])
-        sys.exit(0)
+        except KeyboardInterrupt:
+        
+            colours = colors()
+            print(colours['WARNING'] + '\nUser initiated shutdown' + colours['RESET'])
+            print(colours['BLUE'] + '\nBYE!!' + colours['RESET'])
+            sys.exit(0)
+
+    else:
+
+        try:
+            WeaPy(arg)
+
+        except KeyboardInterrupt:
+        
+            colours = colors()
+            print(colours['WARNING'] + '\nUser initiated shutdown' + colours['RESET'])
+            print(colours['BLUE'] + '\nBYE!!' + colours['RESET'])
+            sys.exit(0)
